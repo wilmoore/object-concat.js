db2b190ca1d5d3994e199ac2ac57d5b4fd36debe
Wil Moore III <wil.moore@wilmoore.com>
2015-04-23 00:32:59 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,75 @@
+'use strict'
+
+/**
+ * exports.
+ */
+
+module.exports = concat
+
+/**
+ * Assign properties of source object(s) to a new object.
+ *
+ * @param {...Object} [sources]
+ * The source objects.
+ *
+ * @param {Function|Null} [iteratee]
+ * Function for transforming target values (must be last parameter).
+ *
+ * @return {String}
+ * string literal.
+ */
+
+function concat (/* [sources], [iteratee] */) {
+  var tee = getIteratee(arguments[arguments.length - 1])
+  var end = arguments.length
+  var idx = -1
+  var out = {}
+
+  while (++idx < end) {
+    copy(out, arguments[idx], tee)
+  }
+
+  return out
+}
+
+/**
+ * Copy source properties to target.
+ *
+ * @param {Object} target
+ * Object to assign properties to.
+ *
+ * @param {Object} source
+ * Object to pull properties from.
+ *
+ * @param {Function|Null} iteratee
+ * Function for transforming target values.
+ *
+ * @return {Object}
+ * Modified target object.
+ */
+
+function copy (target, source, iteratee) {
+  for (var key in Object(source)) {
+    if (iteratee) {
+      target[key] = iteratee(key, target[key], source[key])
+    } else {
+      target[key] = source[key]
+    }
+  }
+
+  return target
+}
+
+/**
+ * Return iteratee if is a `Function`, otherwise, return `null`.
+ *
+ * @param {Function|Object} iteratee
+ * Value that optionally a function.
+ *
+ * @return {Function|Null}
+ * Return iteratee if is a `Function`, otherwise, return `null`.
+ */
+
+function getIteratee (iteratee) {
+  return typeof iteratee === 'function' ? iteratee : null
+}
---
+++ test.js
@@ -0,0 +1,62 @@
+'use strict'
+
+/*!
+ * imports.
+ */
+
+var test = require('tape-catch')
+
+/*!
+ * imports (local).
+ */
+
+var concat = require('./')
+
+/*!
+ * helpers.
+ */
+
+function playerUp (key, targetVal, sourceVal) {
+  return key === 'player' ? sourceVal.toUpperCase() : sourceVal
+}
+
+function ignoreUndefined (key, targetVal, sourceVal) {
+  return sourceVal === undefined ? targetVal : sourceVal
+}
+
+/*!
+ * parameters.
+ */
+
+var parameters = [
+  {
+    name: 'subsequent objects override previous',
+    args: [ { level: 1 }, { player: 'isaac', level: 5 } ],
+    expected: { player: 'isaac', level: 5 }
+  },
+
+  {
+    name: 'iteratee transforms target values based on key',
+    args: [ { level: 1 }, { player: 'isaac', level: 5 }, playerUp ],
+    expected: { player: 'ISAAC', level: 5 }
+  },
+
+  {
+    name: 'iteratee ignores source values based on value match',
+    args: [ { level: 1, karma: 20 }, { player: 'isaac', level: 5, karma: void 0 }, ignoreUndefined ],
+    expected: { player: 'isaac', level: 5, karma: 20 }
+  }
+]
+
+/*!
+ * tests.
+ */
+
+test('concat()', function (t) {
+  t.plan(parameters.length)
+
+  parameters.forEach(function (p) {
+    var actual = concat.apply(null, p.args)
+    t.deepEqual(actual, p.expected, p.name)
+  })
+})
